0bac76e30fdf0a1d77d507477c589ebe2945a271
KATNICE <91684418+KATNICE@users.noreply.github.com>
2021-10-13 23:12:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -11,29 +11,29 @@ Cliquez sur le bouton d'aide ci-dessous.. <i>/help</i> pour plus de détails.</b
 
 
     HELP_MSG = """
-<i>Tu ne peux pas m'ajouter à ton Groupe {}! :)</i>
+<i>Tu ne peux pas m'ajouter à ton Groupe ni m'envoyer de message ici:)</i>
 
 <i><b>
 -> Pour que je t'apporte mon aide! tu doit-être obligatoirement membre👨‍👩‍👦‍👦 de @Shar_Group et pourquoi pas de @Sharing_Club😏✊
 
--> Tu peux aussi Faire tes recherches directement via mon amie Alexa 👉@Alexandrinebot </b></i>
+-> Tu peux aussi faire tes recherches grâce à copine Alexa 👉@Alexandrinebot </b></i>
 
--> Ecrire simplement dans le Groupe @Shar_Group le nom du film ou la série dont tu as besoin.
-
+-> Ecrire simplement dans le Groupe @Shar_Group le nom du film ou la série dont tu as besoin et laisse-moi faire le reste👌
+<b>
 -> N.B: Une seule requête par Message (Si non je pourrais pas trouver automatiquement ce que tu demande)
 
--> Veuillez à l'orthographe correcte des noms des films et des Séries.
-
+-> Veuillez à l'orthographe correcte des noms des films ou Séries.
+</b>
 <b>© @Shar_Group</b>
 """
 
 
     ABOUT_MSG = """⭕️<b>Mon Nom : Katnice </b>
 
-⭕️<b>Créateur :</b> @SharingClub_bot   
+⭕️<b>Créateur :</b> @Philanthrpe   
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Groupe :</b> <a href='https://t.me/Shar_Group/'>Devenir👨‍👩‍👧‍👦</a> 
 
 """
